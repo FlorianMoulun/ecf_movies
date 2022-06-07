d9f42bd571672ca9641b8cbc8859f4d4f8636e76
Moulun Florian <flomoulun@gmx.fr>
2022-06-07 11:43:34 +0200

    [Fixed]
    Nom de variable plus cohérente

--- request/03-getDifference.php
+++ request/03-getDifference.php
@@ -4,8 +4,8 @@
 
 require_once('../connect.php');
 
-$ecart_year = $db->prepare("SELECT MAX(release_year) - MIN(release_year) FROM release_years");
-$ecart_year->execute();
-$res_ecart_year = $ecart_year->fetchAll(PDO::FETCH_ASSOC);
+$getDifference = $db->prepare("SELECT MAX(release_year) - MIN(release_year) FROM release_years");
+$getDifference->execute();
+$res_getDifference = $getDifference->fetchAll(PDO::FETCH_ASSOC);
 
-/*print_r($res_ecart_year);*/
+/*print_r($res_getDifference);*/
